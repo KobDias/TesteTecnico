9f2e85d649787957514b3576f5177174aa26562d
KobDias <sofhia.dias@etec.sp.gov.br>
2025-04-13 13:15:53 -0300

    hash

--- app.py
+++ app.py
@@ -6,10 +6,21 @@ from models import Cliente
 import hashlib
 
 app = Flask(__name__)
+app.secret_key = "m04H4H4"
+lm = LoginManager(app)
+lm.login_view = 'login'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cabeleleila.db'
+db.init_app(app)
+
+def hash(txt):
+    hash_obj = hashlib.sha256(txt.encode('utf-8'))
+    return hash_obj.hexdigest()
 
 @app.route('/')
 def index():
     return render_template('header.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
